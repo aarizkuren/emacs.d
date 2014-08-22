b3c8a26b02754f2151f3243baceb22941210b6c0
Asier Arizkuren <asier@arizkuren.net>
2014-08-22 14:04:50 +0200

    Adding some local configurations: org mode configs and no-backup files

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -8,6 +8,15 @@
 (set-face-attribute 'default nil :font "Source Code Pro Medium")
 (set-frame-font "Source Code Pro Medium" nil t)
 
+; Disable auto-save and backups
+(setq backup-inhibited t)
+(setq auto-save-default nil)
+
+;; ORG Configurations
+(setq org-agenda-files (list "~/.org/burlata.org"
+                             "~/.org/lana.org"
+                             "~/.org/proiektuak.org"
+                             ))
 
 (provide 'init-local)
 ;;; init-local.el ends here
